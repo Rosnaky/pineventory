dd39808d59b93e05a2827804f2873f4558e48f40
Rosnaky <ronakjpatelr22@gmail.com>
2026-04-06 19:34:23 -0400

    Add proper retry logci

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 import sqlite3
 import os
 from app.bot import Bot
-from app.config import DISCORD_TOKEN, INIT_RETRY_DELAY_S, RETRY_DELAY_S_MULTIPLIER
+from app.config import DISCORD_TOKEN, INIT_RETRY_DELAY_S, MAX_RETRY, RETRY_DELAY_S_MULTIPLIER
 from app.utils.logger import logger
 from app.web_server.web_server import start_web_server
 
@@ -16,11 +16,14 @@ async def main():
 
     web_runner = await start_web_server()
 
-    bot = Bot()
 
     retry_delay = INIT_RETRY_DELAY_S
-    while True:
+    current_retry = 0
+    while current_retry < MAX_RETRY:
+        current_retry += 1
+        bot = Bot()
         try:
+            # Blocking loop
             await bot.start(DISCORD_TOKEN) # type: ignore
         except discord.HTTPException as e:
             if e.status == 429:
@@ -28,17 +31,18 @@ async def main():
                 await asyncio.sleep(retry_delay)
                 retry_delay *= RETRY_DELAY_S_MULTIPLIER;
             else:
-                logger.error(f"HTTP Error during bot startup: {e}")
+                logger.warning(f"HTTP Error during bot startup: {e}")
                 raise
         except Exception as e:
-            logger.error(f"Error during bot startup: {e}")
+            logger.warning(f"Error during bot startup: {e}")
             raise
         finally:
             if not bot.is_closed():
                 await bot.close()
-            await web_runner.cleanup()
 
-    logger.info("Initialization successful")
+    await web_runner.cleanup()
+
+    logger.info("Initialization failed")
 
 
 if __name__ == "__main__":
